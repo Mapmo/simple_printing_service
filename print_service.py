577d52957383012dbcc156d947acb187a413f81f
Mapmo <mapmoeu@gmail.com>
2023-10-24 10:59:26 +0300

    Add a python service that is printing an environment variable at regular intervals

---
+++ print_service.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env  python3
+
+import argparse
+import time
+import os
+
+parser = argparse.ArgumentParser(
+    prog='SimpleService',
+    description='This program is printing an environment variable that is optionally specified',
+)
+
+defaultVariableName = "DEFAULT"
+defaultInterval = 10
+parser.add_argument('-n', '--variableName', default=defaultVariableName)
+parser.add_argument('-i', '--interval', default=10)
+
+
+args = parser.parse_args()
+
+while True:
+    try:
+        print(os.environ[args.variableName])
+        time.sleep(args.interval)
+    except KeyError:
+        print(f"Environment variable {args.variableName} is not set")
+        print("Exiting...")
+        break
